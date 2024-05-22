b685362468a7f25b88bd85a9d645a1fdd996e093
VLE\arisa.b <arisa.b@valeuraenergy.com>
2024-05-23 02:25:21 +0700

    📝 Clean code

--- Web/ClientApp/src/app/feature/db/db.module.ts
+++ Web/ClientApp/src/app/feature/db/db.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { ListGroupComponent } from "./dbmt04/list-group/list-group.component";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "@app/shared/shared.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DbRoutingModule } from "./db-routing.module";
 import { LazyTranslationService } from "@app/core/services/lazy-translation.service";
 import { ListGroupDetailComponent } from './dbmt04/list-group-detail/list-group-detail.component';
@@ -21,8 +20,6 @@ import { Dbmt04Service } from "./dbmt04/dbmt04.service";
         CommonModule,
         DbRoutingModule,
         SharedModule,
-        FormsModule,
-        ReactiveFormsModule,
     ],
     providers: [
         Dbmt04Service
--- Web/ClientApp/src/app/feature/su/su.module.ts
+++ Web/ClientApp/src/app/feature/su/su.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "@app/shared/shared.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SuRoutingModule } from "./su-routing.module";
 import { LazyTranslationService } from "@app/core/services/lazy-translation.service";
 
@@ -13,8 +12,6 @@ import { LazyTranslationService } from "@app/core/services/lazy-translation.serv
         CommonModule,
         SuRoutingModule,
         SharedModule,
-        FormsModule,
-        ReactiveFormsModule,
     ],
     providers: [
 
