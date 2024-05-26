28908f9d15aa8f842282bbdfd248c257e662389f
Chayanin <33849student1@gmail.com>
2024-05-26 23:36:46 +0700

    still bug

--- Web/ClientApp/src/app/feature/db/db-routing.module.ts
+++ Web/ClientApp/src/app/feature/db/db-routing.module.ts
@@ -7,6 +7,9 @@ import { ListGroupDetailResolverService } from "./dbmt04/list-group-detail/list-
 import { ListValueComponent } from "./dbmt04/list-value/list-value.component";
 import { ListValueDetailComponent } from "./dbmt04/list-value-detail/list-value-detail.component";
 import { ListValueDetailResolverService } from "./dbmt04/list-value-detail/list-value-detail.resolver";
+import { LangListComponent } from "./dbmt20/lang-list/lang-list.component";
+import { LangDetailComponent } from "./dbmt20/lang-detail/lang-detail.component";
+import { langDetailResolver } from "./dbmt20/lang-detail/lang-detail.resolver";
 
 const routes: Routes = [
     {
