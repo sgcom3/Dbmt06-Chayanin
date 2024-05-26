b727f6c73b7857fcf4252bc4cb0ba867813f0e02
Chayanin <33849student1@gmail.com>
2024-05-26 23:36:46 +0700

    newbug

--- Web/ClientApp/src/app/feature/db/db-routing.module.ts
+++ Web/ClientApp/src/app/feature/db/db-routing.module.ts
@@ -7,6 +7,9 @@ import { ListGroupDetailResolverService } from "./dbmt04/list-group-detail/list-
 import { ListValueComponent } from "./dbmt04/list-value/list-value.component";
 import { ListValueDetailComponent } from "./dbmt04/list-value-detail/list-value-detail.component";
 import { ListValueDetailResolverService } from "./dbmt04/list-value-detail/list-value-detail.resolver";
+import { LangListComponent } from "./dbmt20/lang-list/lang-list.component";
+import { LangDetailComponent } from "./dbmt20/lang-detail/lang-detail.component";
+import { langDetailResolver } from "./dbmt20/lang-detail/lang-detail.resolver";
 
 const routes: Routes = [
     {
@@ -36,8 +39,23 @@ const routes: Routes = [
                 resolve: { dbmt04: ListValueDetailResolverService },
                 data: { code: 'dbmt04' },
             },
+
            
             
+
+            {
+                path: 'dbmt20',
+                component: LangListComponent,
+                data: { code: 'dbmt20' },
+            },
+            {
+                path: 'dbmt20/detail',
+                component: LangDetailComponent,
+                canDeactivate: [CanDeactivate],
+                resolve: { dbmt20: langDetailResolver },
+                data: { code: 'dbmt20' },
+            },
+
         ]
     }
 ]
